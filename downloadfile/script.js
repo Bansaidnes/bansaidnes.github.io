b14547757bed2a7249f604c5a4fe3a3b89d91090
Bansaidnes <yugenkc6@gmail.com>
2022-08-25 17:07:05 +0400

    E

--- downloadfile/script.js
+++ downloadfile/script.js
@@ -1,6 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 console.log("ID: ", urlParams.get('id'));
-var Url = 'https://fa3f-20-58-35-110.eu.ngrok.io/api/publicfiledownload?id=' + urlParams.get('id');
+var Url = 'https://1b56-20-58-35-110.eu.ngrok.io/api/publicfiledownload?id=' + urlParams.get('id');
 
 console.log(Url);
 
