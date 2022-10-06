f2583d7349b1c69f4c4bed9e9a0b9af155db5388
Bansaidnes <yugenkc6@gmail.com>
2022-10-06 19:29:43 +0400

    finally fix CORS

--- downloadfile/script.js
+++ downloadfile/script.js
@@ -1,15 +1,34 @@
 const urlParams = new URLSearchParams(window.location.search);
-var fileName = urlParams.get('filename');
 var id = urlParams.get('id');
-if(fileName == null) fileName = "[Default]ripwin.png";
-if(id == null) id = 56348970;
 
-var Url = 'https://1b56-20-58-35-110.eu.ngrok.io/api/publicfiledownload?id=' + id;
+if(id == null) id = 67099983;
+
+var Url = 'https://d8bd-20-219-217-173.in.ngrok.io/api/publicfiledownload?id=' + id;
+var Url2 = 'https://d8bd-20-219-217-173.in.ngrok.io/api/getfileinfo?id=' + id;
+
+var filename = getName();
 
 console.log(Url);
+console.log(filename)
 
 function Download(){
-    window.open(Url, "_self");
+    var link = document.createElement("a");
+    link.download = filename;
+    link.href = Url2;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    delete link;
+}
+function getName(){
+    fetch(Url)
+            .then(function(response)
+            {
+              return response.json();
+            }).then(function(response)
+            {
+              document.getElementById("download").innerHTML = "You are downloading: "+response.filename;
+            });
 }
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
